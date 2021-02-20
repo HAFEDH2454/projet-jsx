ebadf48ec0f7f8842e466f8043092dd60b834b02
HAFEDH2454 <71188499+HAFEDH2454@users.noreply.github.com>
2021-02-20 15:43:25 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
 
+
+
+import srcimage from "./src/imageinSrc.png"
+    
+    
+function App()
+{
+    return (
+        <div className="App">
+            <header className="App-header">
+
+            <title>conversion en jsx</title>
+
+            <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+                 <h1 className="title red">Your name here</h1>
+       
+                 <br ></br>
+       
+                 <img src={srcimage} alt="imageInsrc" >
+       
+                 <br ></br>
+       
+                 <img src="./public/imageInpublic.png" >
+        
+             </div>
+       
+             <video width={320} height={240} controls></video>
+       
+                <source src="myVideo.mp4" type="video/mp4" >
+       
+              </video>
+       </header>
+       </div>
+       )
+ }
+ 
+export default App;
 export default App;
